06e8d513dec7f02db69b90595b0ef8a364b0e3d4
Ricardo Mello <rimello@squaretrade.com>
2021-02-17 18:37:02 -0300

    feat: generate icon component

---
+++ src/app/icon/icon.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-icon',
+  template: `
+    <p>
+      icon works!
+    </p>
+  `,
+  styleUrls: ['./icon.component.scss']
+})
+export class IconComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
