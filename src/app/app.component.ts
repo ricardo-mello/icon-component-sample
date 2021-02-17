db529cd8b81afc6a027f6fd32d307e4a4c681909
Ricardo Mello <rimello@squaretrade.com>
2021-02-17 17:10:25 -0300

    feat: add player

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'icon-component-sample';
+  action = 'play';
+
+  alternate() {
+    this.action = this.action === 'play' ? 'pause' : 'play';
+  }
 }
