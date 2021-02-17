84eb7a5f4d531ecafb6ea2b3a1821e201ef9d7cf
Ricardo Mello <rimello@squaretrade.com>
2021-02-17 18:57:22 -0300

    feat: icon component

--- src/app/icon/icon.component.ts
+++ src/app/icon/icon.component.ts
@@ -1,19 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+// src/app/icon/icon.component.ts
+import { ChangeDetectionStrategy, Component, HostBinding, Input, ViewEncapsulation } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { Observable } from 'rxjs';
+import { IconService } from './icon.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-icon',
-  template: `
-    <p>
-      icon works!
-    </p>
-  `,
-  styleUrls: ['./icon.component.scss']
+  // O template simplesmente renderiza o SVG baixado em uma div interna
+  template: `<div class="icon-inner" [innerHTML]="svgContent | async"></div>`,
+  styleUrls: ['./icon.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.ShadowDom
 })
-export class IconComponent implements OnInit {
+export class IconComponent {
+  @Input()
+  @HostBinding('class')
+    // Classes com tamanhos predefinidos
+  size: 'small' | 'large' | 'default' = 'default';
 
-  constructor() { }
+  // Variável com a requisição do SVG que vai ser renderizado
+  public svgContent: Observable<SafeHtml> | undefined;
 
-  ngOnInit(): void {
+  constructor(
+    private sanitizer: DomSanitizer,
+    private iconService: IconService
+  ) {}
+
+  @Input()
+  set src(value: string) {
+    // Pegamos o caminho do SVG e invocamos o
+    // nosso service que vai baixá-lo.
+    this.setSvgContent(value);
   }
 
+  private setSvgContent(src: string): void {
+    // Baixamos o SVG do service e atribuímos
+    // à nossa variável que é renderizada no template
+    this.svgContent = this.iconService
+      .getSvgContent(src)
+      .pipe(map(content => this.sanitizer.bypassSecurityTrustHtml(content)));
+  }
 }
